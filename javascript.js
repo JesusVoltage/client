9e72359934bc6606f4fdba5dc9cd5139567d2452
JesusVoltage <jesusreyesreal@gmail.com>
2019-01-30 22:49:54 +0100

    primer dinamismo, de consola solo

--- javascript.js
+++ javascript.js
@@ -12,21 +12,26 @@ $(document).ready(function () {
         success: function (datos) {
             glifos = datos;
             $(document).ready(function () {
-                let contador = 0;
+                var contador = 0;
                 for (let j = 0; j < 7; j++) {
                     
                     $('#content').append('<div id="'+j+'" class="row my-1"></div>');
 
                     for (i = j; i < 10; i++) {
                         contador++;
+                        console.log(contador);
                         let letrita = glifos[j].solucion.split("");
                         //let numerito = letraNumero(letrita[i-j]);
-                        $('#'+j).append('<div class="all d-inline text-center"><p class="text-center">'+numerito[letrita[i-j]]+'</p><input class="boton"></div></div>');
+                        $('#'+j).append('<div class="all d-inline text-center"><p id="'+(contador+50)+'" class="text-center">'+numerito[letrita[i-j]]+'</p><input class="boton"></div></div>');
                     }
 
                     $('#content').append('<p class="text-left">' + glifos[j].pista + '</p>');
 
                 }
+                $("input").on('keyup', function(){
+                    var value = $(this).val();
+                    console.log(numerito[value]);
+                }).keyup();
             });
         }
     });
