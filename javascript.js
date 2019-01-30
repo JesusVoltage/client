2f10c8c696a624ade295bb5d6215d990a6ef4d83
JesusVoltage <jesusreyesreal@gmail.com>
2019-01-30 14:45:21 +0100

    last changes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,30 @@
-$( document ).ready(function() {
-    $('#filas').css();
+var glifos;
+
+$(document).ready(function(){
+
+    $.ajax({
+        url: 'glifos.json',
+        dataType: 'json',
+        success: function(datos){
+            glifos = datos;
+            $( document ).ready(function() {
+
+                for(j = 0  ; j < 7 ; j++){
+                    
+                    for(i = j ; i < 10 ; i++ ){
+                        $('#content').append('<input class="boton"></input>');
+                        
+                        
+                        
+                        
+                    }
+                    $('#content').append('<p>'+glifos[j].pista+'</p>');
+                }
+            });
+        }
+    });
 });
+
+
+var contador = 0;
+
